0827a7d12129327da6e12797692136ac5a8cb484
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-11 02:30:00 -0400

    Added button and basic site styling

---
+++ TarkovSauce.Client.Components/Buttons/TSButton.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TarkovSauce.Client.Components.Buttons
+{
+    public partial class TSButton
+    {
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+        [Parameter]
+        public string Content { get; set; } = "";
+        [Parameter]
+        public bool IsPrimary { get; set; }
+        protected override string CssImpl => IsPrimary ? $"primary {base.CssImpl}" : base.CssImpl;
+    }
+}
