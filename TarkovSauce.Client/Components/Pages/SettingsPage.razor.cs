163d3c676a89240c2d0ebc450d255324160717d5
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-11 06:12:43 -0400

    Implemented app settings with auto refresh through IConfiguration

---
+++ TarkovSauce.Client/Components/Pages/SettingsPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using TarkovSauce.Client.Utils;
+
+namespace TarkovSauce.Client.Components.Pages
+{
+    public partial class SettingsPage
+    {
+        [Inject]
+        public IAppDataManager AppDataManager { get; set; } = default!;
+        private AppDataJson? _appData;
+        protected override void OnInitialized()
+        {
+            _appData = AppDataManager.GetAppData();
+        }
+        private void OnSaveEvent()
+        {
+            if (_appData is null)
+            {
+                return;
+            }
+
+            AppDataManager.WriteAppData(_appData);
+        }
+    }
+}
