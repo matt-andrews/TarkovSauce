b63aa4e4156810924e12e81f16cfc59389f5d0c6
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-11 03:32:46 -0400

    Added text field

--- TarkovSauce.Client.Components/Buttons/TSButton.razor.cs
+++ TarkovSauce.Client.Components/Buttons/TSButton.razor.cs
@@ -10,6 +10,6 @@ namespace TarkovSauce.Client.Components.Buttons
         public string Content { get; set; } = "";
         [Parameter]
         public bool IsPrimary { get; set; }
-        protected override string CssImpl => IsPrimary ? $"primary {base.CssImpl}" : base.CssImpl;
+        protected override string CssImpl => IsPrimary ? $"tsbutton primary {base.CssImpl}" : $"tsbutton {base.CssImpl}";
     }
 }
---
+++ TarkovSauce.Client.Components/Inputs/TSInput.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace TarkovSauce.Client.Components.Inputs
+{
+    public partial class TSInput
+    {
+        private readonly string _id = Guid.NewGuid().ToString();
+        [Parameter]
+        public string Placeholder { get; set; } = "";
+        [Parameter]
+        public string Value { get; set; } = "";
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+    }
+}
