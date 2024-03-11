d59686ec42978a635cf55b9fb30f71f24d6a7506
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-11 04:12:23 -0400

    Added checkbox and gear icon button

---
+++ TarkovSauce.Client.Components/Buttons/ButtonBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TarkovSauce.Client.Components.Buttons
+{
+    public class ButtonBase : StyleableComponentBase
+    {
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+        [Parameter]
+        public string Label { get; set; } = "";
+    }
+}
---
+++ TarkovSauce.Client.Components/Buttons/ToggleButtonBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace TarkovSauce.Client.Components.Buttons
+{
+    public class ToggleButtonBase : ButtonBase
+    {
+        [Parameter]
+        public bool IsOn { get; set; }
+        [Parameter]
+        public EventCallback<bool> IsOnChanged { get; set; }
+        protected async Task OnToggleEvent()
+        {
+            IsOn = !IsOn;
+            await IsOnChanged.InvokeAsync(IsOn);
+            await OnClick.InvokeAsync();
+        }
+    }
+}
