a76ef157e0b9eeecb6f6319f852d12051aa6f290
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-12 18:58:13 -0400

    Implemented tarkov tracker auto task update

--- TarkovSauce.Client/Data/Models/Remote/TaskStatusBody.cs
+++ TarkovSauce.Client/Data/Models/Remote/TaskStatusBody.cs
@@ -2,30 +2,24 @@
 {
     public class TaskStatusBody
     {
-        public string? Id { get; set; }
+        public string? Id { get; private set; }
         public string State { get; private set; }
-        private TaskStatusBody(string newState)
+        private TaskStatusBody(string id, string newState)
         {
+            Id = id;
             State = newState;
         }
-        public static TaskStatusBody Completed => new("completed");
-        public static TaskStatusBody Uncompleted => new("uncompleted");
-        public static TaskStatusBody Failed => new("failed");
-        public static TaskStatusBody From(TaskStatus code)
+        public static TaskStatusBody From(string id, TaskStatus code)
         {
             if (code == TaskStatus.Finished)
             {
-                return TaskStatusBody.Completed;
+                return new TaskStatusBody(id, "completed");
             }
             if (code == TaskStatus.Failed)
             {
-                return TaskStatusBody.Failed;
+                return new TaskStatusBody(id, "failed");
             }
-            return TaskStatusBody.Uncompleted;
-        }
-        public static TaskStatusBody From(MessageType messageType)
-        {
-            return TaskStatusBody.From((TaskStatus)messageType);
+            return new TaskStatusBody(id, "uncompleted");
         }
     }
 }
