f375b2f4ede8c8b55df7cd8396c192909416b0b8
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-18 12:48:04 -0400

    Added automatic position when location logged (usually around raid start)

---
+++ TarkovSauce.Client/Data/MessageListeners/SpawnPointListener.cs
@@ -0,0 +1,15 @@
+﻿using TarkovSauce.Client.Data.Providers;
+
+namespace TarkovSauce.Client.Data.MessageListeners
+{
+    internal class SpawnPointListener(IRawLogProvider _rawLogProvider, IScreenshotWatcherProvider _screenshotWatcherProvider) : BaseMessageListener(_rawLogProvider)
+    {
+        public override string Match => "application|Reason:PacketsQueue";
+
+        protected override void OnEventImpl(string str)
+        {
+            var pos = str.Split('(')[1].Split(')')[0].Split(',');
+            _screenshotWatcherProvider.NewScreenshot(pos);
+        }
+    }
+}
