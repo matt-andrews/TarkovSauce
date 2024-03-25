3c58fe0116b2019f0bb6a90c489ada151a0fce48
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-03-25 02:02:37 -0400

    Added launcher project

---
+++ TarkovSauce.Launcher/Manifest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TarkovSauce.Launcher
+{
+    internal class Manifest
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; set; } = "";
+        [JsonPropertyName("versionId")]
+        public int VersionId { get; set; }
+    }
+}
---
+++ TarkovSauce.Launcher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using TarkovSauce.Launcher;
+
+string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sauce App", "Sauce App");
+string exe = Path.Combine(path, "TarkovSauce.Client.Exe");
+string manifest = Path.Combine(path, "manifest.json");
+
+var httpClient = new BuildHttpClient();
+
+var jumbler = new ConsoleJumbler();
+await jumbler.WriteLine("Welcome to Tarkov Sauce");
+
+await jumbler.Write("Checking for updates");
+
+Manifest remoteManifest = await jumbler.Load(() => httpClient.Get<Manifest>("manifest.json"));
+
+bool update = false;
+if (Directory.Exists(path) && File.Exists(exe) && File.Exists(manifest))
+{
+    using var fs = new FileStream(manifest, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    var manifestObj = JsonSerializer.Deserialize<Manifest>(fs);
+    if (manifestObj?.VersionId != remoteManifest.VersionId)
+    {
+        update = true;
+    }
+}
+else
+{
+    update = true;
+}
+
+if (update)
+{
+    await jumbler.Write("Updating");
+    await jumbler.Load(() => httpClient.DownloadZip($"Sauce App.{remoteManifest.Version}.zip"));
+}
+
+await jumbler.WriteLine("Launching...");
+Process.Start(exe);
+await Task.Delay(1000);
